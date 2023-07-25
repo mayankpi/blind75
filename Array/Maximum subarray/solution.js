7377586cd9381e86893b8b6b0b9d87c387521480
mayankpi <mayankm522@gmail.com>
2023-07-25 18:55:50 +0530

    add maximum subarray

---
+++ Array/Maximum subarray/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let max = -Infinity;
+    let meh = 0;
+    for (let i = 0; i < nums.length; i++) {
+        meh += nums[i];
+        if (meh > max) {
+            max = meh;
+        }
+        if (meh < 0) {
+            meh = 0;
+        }
+    }
+    return max;
+};
